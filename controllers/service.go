c24a033d0a3888230211fc5d9fe6839382ea90af
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2020-05-30 22:55:09 +0300

    adding post-method

--- controllers/service.go
+++ controllers/service.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
 	"regexp"
+
+	"../models"
 )
 
 type serviceController struct {
@@ -10,7 +13,40 @@ type serviceController struct {
 }
 
 func (sc serviceController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("container state service..."))
+	if r.URL.Path == "/services" {
+		switch r.Method {
+		case http.MethodPost:
+			sc.post(w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	}
+}
+
+func (sc *serviceController) post(w http.ResponseWriter, r *http.Request) {
+	s, err := sc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse User object"))
+		return
+	}
+	s, err = models.AddService(s)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJSON(s, w)
+}
+
+func (sc *serviceController) parseRequest(r *http.Request) (models.Service, error) {
+	dec := json.NewDecoder(r.Body)
+	var s models.Service
+	err := dec.Decode(&s)
+	if err != nil {
+		return models.Service{}, err
+	}
+	return s, nil
 }
 
 func newServiceController() *serviceController {
